0b2babe394b6468ce481415af72541dadd0c0a16
duy ha <duyhph@gmail.com>
2019-10-23 21:29:39 +0700

    Update auth.go

--- auth.go
+++ auth.go
@@ -88,7 +88,7 @@ func main() {
 	var port int
 	var debug bool
 	flag.IntVar(&port, "port", 9877, "help message for flagname")
-	flag.BoolVar(&debug, "debug", true, "Indicates  if debug messages should be printed in log files")
+	flag.BoolVar(&debug, "debug", false, "Indicates  if debug messages should be printed in log files")
 	flag.Parse()
 
 	logLevel := log.DebugLevel
